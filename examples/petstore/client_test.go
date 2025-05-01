e2485463a4ecfc20bd66edf78f2ef5a0db502f18
Ash Berlin-Taylor <ash_github@firemirror.com>
2025-05-01 22:38:31 +0100

    First pass of resty templates to override the behaviour of oapi-codegen
    I'm sure there will be more edge cases I haven't discovered yet, but this
    works for the few simple test cases in the Petstore spec, and it compiles
    which is a start!

--- examples/petstore/client_test.go
+++ examples/petstore/client_test.go
@@ -1,13 +1,60 @@
 package petstore
 
 import (
+	"context"
+	"net/http"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/jarcoal/httpmock"
+	"github.com/stretchr/testify/suite"
 )
 
-func TestNewClient(t *testing.T) {
-	client, err := NewClient("a")
-	assert.Nil(t, err)
-	assert.NotNil(t, client)
+type DryRunTransport struct {
+	Handler func(*http.Request) (*http.Response, error)
+}
+
+func (dr *DryRunTransport) RoundTrip(r *http.Request) (*http.Response, error) {
+	return dr.Handler(r)
+}
+
+type PetStoreSuite struct {
+	suite.Suite
+	transport *DryRunTransport
+	client    ClientInterface
+}
+
+func (suite *PetStoreSuite) SetupTest() {
+	var err error
+	suite.transport = &DryRunTransport{}
+	suite.client, err = NewClient("http://invalid.localdomain/", WithRoundTripper(suite.transport))
+	suite.Require().Nil(err)
+}
+
+func TestPetStoreSuite(t *testing.T) {
+	suite.Run(t, new(PetStoreSuite))
+}
+
+func (suite *PetStoreSuite) TestAdd() {
+	suite.transport.Handler = func(r *http.Request) (*http.Response, error) {
+		return httpmock.NewJsonResponse(200, map[string]any{
+			"id":        123,
+			"name":      "Midnight",
+			"photoUrls": nil,
+			"status":    "sold",
+		})
+	}
+
+	resp, err := suite.client.Pet().Add(context.Background())
+	suite.Nil(err)
+	suite.NotNil(resp)
+	suite.EqualValues(*resp.Id, 123)
+	suite.Equal(resp.Name, "Midnight")
+}
+
+func (suite *PetStoreSuite) TestDelete() {
+	suite.transport.Handler = func(r *http.Request) (*http.Response, error) {
+		return httpmock.NewBytesResponse(200, []byte{}), nil
+	}
+	err := suite.client.Pet().Delete(context.Background(), 0, nil)
+	suite.Nil(err)
 }
--- pkg/generator/generator.go
+++ pkg/generator/generator.go
@@ -1,25 +1,74 @@
 package generator
 
-import "github.com/oapi-codegen/oapi-codegen/v2/pkg/codegen"
+import (
+	"embed"
+	"fmt"
+	"path"
 
-func GenerateTypesGo() ([]byte, error) {
-	ccfg := codegen.Configuration{
-		PackageName: "a",
-		Compatibility: codegen.CompatibilityOptions{
-			AlwaysPrefixEnumValues: true,
-		},
-		Generate: codegen.GenerateOptions{
-			Models: true,
-		},
-		OutputOptions: codegen.OutputOptions{
-			SkipPrune: true,
-			UserTemplates: map[string]string{
-				"imports.tmpl": "A",
-			},
-		},
+	"github.com/getkin/kin-openapi/openapi3"
+	"github.com/oapi-codegen/oapi-codegen/v2/pkg/codegen"
+	"github.com/oapi-codegen/oapi-codegen/v2/pkg/util"
+)
+
+//go:embed templates/*
+var f embed.FS
+
+type GenerateArgs struct {
+	codegen.Configuration `yaml:",inline"`
+
+	// OutputFile is the filename to output.
+	OutputFile string `yaml:"output,omitempty"`
+	// Input filename is file (or URL) to read input from
+	Input string `yaml:"input,omitempty"`
+	// Spec is to provide a loaded spec already. Mutually exclusive with Input
+	Spec *openapi3.T
+}
+
+func Generate(opts GenerateArgs) (string, error) {
+	templates := map[string]string{}
+
+	if err := opts.Validate(); err != nil {
+		return "", fmt.Errorf("configuration error: %w", err)
 	}
 
-	codegen.Generate(nil, ccfg)
+	overlayOpts := util.LoadSwaggerWithOverlayOpts{
+		Path: opts.OutputOptions.Overlay.Path,
+		// default to strict, but can be overridden
+		Strict: true,
+	}
 
-	return nil, nil
+	if opts.OutputOptions.Overlay.Strict != nil {
+		overlayOpts.Strict = *opts.OutputOptions.Overlay.Strict
+	}
+	opts.AdditionalImports = append(opts.AdditionalImports, codegen.AdditionalImport{Package: "resty.dev/v3"})
+
+	if opts.Input != "" {
+		var err error
+		opts.Spec, err = util.LoadSwaggerWithOverlay(opts.Input, overlayOpts)
+		if err != nil {
+			return "", fmt.Errorf("error loading swagger spec in %q\n: %w", opts.Input, err)
+		}
+	}
+	if opts.Spec == nil {
+		return "", fmt.Errorf("one of spec or input filename must be provided")
+	}
+
+	dirName := "templates"
+	entries, err := f.ReadDir(dirName)
+	if err != nil {
+		return "", fmt.Errorf("unable to read templates: %w", err)
+	}
+	for _, entry := range entries {
+		if entry.IsDir() {
+			continue
+		}
+		content, err := f.ReadFile(path.Join(dirName, entry.Name()))
+		if err != nil {
+			return "", fmt.Errorf("unable to read template: %w", err)
+		}
+		templates[entry.Name()] = string(content)
+	}
+	opts.OutputOptions.UserTemplates = templates
+
+	return codegen.Generate(opts.Spec, opts.Configuration)
 }
